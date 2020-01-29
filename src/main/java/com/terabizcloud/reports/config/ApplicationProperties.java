c5decb3cc2c0e5387d20a2fc8e9cca5c1837bb9f
Aliaksei Shalak <aliaksei.shalak@rednavis.com>
2020-01-29 17:55:36 +0300

    Added not Db microservices generated with JHipster.

---
+++ src/main/java/com/terabizcloud/reports/config/ApplicationProperties.java
@@ -0,0 +1,13 @@
+package com.terabizcloud.reports.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Reports.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+}
