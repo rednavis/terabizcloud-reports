8a65372ffdf72cce55a9cf75c0d84cad8769818d
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2020-02-03 11:23:45 +0300

    Remove old projects

--- src/main/java/com/terabizcloud/reports/security/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring Security configuration.
- */
-package com.terabizcloud.reports.security;
--- src/main/java/com/terabizcloud/reports/web/rest/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring MVC REST controllers.
- */
-package com.terabizcloud.reports.web.rest;
